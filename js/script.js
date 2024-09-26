e8f53e69e83d56904a96a21ca6c4d19b7ce79aa2
TukuNoni71 <Adonisjzepeda@gmail.com>
2024-09-26 11:20:44 -0400

    new changes

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 //grabbing From docs
-const addBook = document.querySelector('#book-info');
+const addBookBtn = document.querySelector('#add-book');
 // const rating = document.querySelector('#add-rating')
-const bookModal = document.querySelector('#book-modal');
+const addModal = document.querySelector('#add-modal');
 
 
 /*FUNCTIONS*/
+// Create a function that returns a books array from local storage by the key of books (make sure to JSON.parse the array)
+// If there is not a books array stored to local storage return an empty array
+//
+
 
 //asks for the title and author of the book
-function bookInfo() {
-    let askTitle = prompt("Please provide book title.");
-    console.log(askTitle);
-    let askAuthor = prompt("Please provide the author.");
-    console.log(askAuthor);
+function addBook() {
+    const titleInput = document.querySelector('#title-input');
+    const authorInput = document.querySelector('#author-input');
+    const ratingInput = document.querySelector('#rating-input');
+    const rating = ratingInput.value;
+    console.log(titleInput.value);
+    if (rating <= 5 && rating >= 1) {
 
 
-    let askRating = prompt("How would you rate this book?")
-    console.log(askRating);
-
-<<<<<<< HEAD
-    // if(askRating >= 5 && !< 1){
-=======
-    if(askRating <= 5 && askRating >= 1){
->>>>>>> da016600ee995e7604dcb9a7f5935010e2e79f34
-        
-
-    // }
-
+    }
+    //Create a book object with the three properties corresponding with the three inputs above and the values
+    //Call function above that returns books array, store the array that it returns in a variable
+    //Push the Book object  to the books array variable 
+    //Set the local sotrage books key to the books array above (make sure to JSON.stringify the array)
+    // Clear out the inputs - IE. titleInput.value = ''
 }
 
-addBook.addEventListener('click', bookInfo);
+addBookBtn.addEventListener('click', addBook);
 
 
